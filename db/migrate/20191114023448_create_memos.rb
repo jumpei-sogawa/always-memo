888cc58c0995c0cd2f7cb0a0d5844929c8303ac5
jumpei-sogawa <jumpe123oon@gmail.com>
2019-11-14 11:44:00 +0900

    develop memo app

---
+++ db/migrate/20191114023448_create_memos.rb
@@ -0,0 +1,11 @@
+class CreateMemos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memos do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_011038) do
+ActiveRecord::Schema.define(version: 2019_11_14_023448) do
+
+  create_table "memos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
